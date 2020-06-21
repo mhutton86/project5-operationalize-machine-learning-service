4db6e3836f45651361a6df2873133c2ed59cc9d4
Mark-Anthony Hutton <mhutton86@gmail.com>
2020-06-21 02:58:23 +0000

    Updated run_kubernetes.sh to run project docker image.

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -2,17 +2,24 @@
 
 # This tags and uploads an image to Docker Hub
 
+username=mhutton86
+dockername=boston-house-price-prediction
+
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=${username}/${dockername}
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run ${dockername}\
+    --generator=run-pod/v1\
+    --image=$dockerpath\
+    --port=80 --labels app=${dockername}
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward ${dockername} 8000:80
